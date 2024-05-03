170f9e86d839478ef1ba1f50d36ca2acc52de80f
gabyr <gabrielitorg@gmail.com>
2024-05-03 10:03:56 -0300

    se definio dia como tipo numerico y se creo un swtich

---
+++ assets/js/diaSemana.js
@@ -0,0 +1,28 @@
+let dia= parseInt(prompt ("ingrese un numero del 1 al 7"));
+
+switch (dia) {
+    case 1:
+        console.log("es lunes")
+        break;
+    case 2:
+        console.log ("es martes")
+        break;
+    case 3:
+        console.log ("es miercoles")
+        break; 
+    case 4:
+        console.log ("es jueves")
+        break; 
+    case 5:
+        console.log ("es viernes")
+        break;
+    case 6:
+        console.log ("es sabado")
+        break;
+    case 7:
+        console.log ("es domingo")
+        break;
+    case 8:
+        console.log ("no es un dia de la semana")
+        break;    
+}
